92e8d9f81a3f206e89a16df76a532f84ba978beb
ImmortalxHyena <ImmortalxHyena@gmail.com>
2023-09-06 10:00:53 +1200

    Removed old saves. Changed drilling back to normal.

--- drilling.py
+++ drilling.py
@@ -1,7 +1,6 @@
 import time
 import random
 import math
-from colorama import init, Fore, Style
 
 # Function to clear the console (for better animation)
 def clear_console():
@@ -14,20 +13,17 @@ def display_image(image):
         print(row)
 
 # Function to add debris around the laser beam
-def add_debris(image, debrisX, debrisY):
-    for i in range(debrisX, debrisY):
+def add_debris(image, debrisZoneX, debrisZoneY):
+    for i in range(debrisZoneX, debrisZoneY):
         row = list(image[i])
         for j in range(10, 15):
             if random.random() < 0.3:  # Adjust the probability as desired
-                randomColor = random.choice([Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE])
-                debris_char = random.choice(['.', ',', ':', ';', "'", "\""])
-                row[j] = f"{randomColor}{debris_char}{Style.RESET_ALL}"
+                row[j] = random.choice([".", ",", ":", ";", "'", "\""])
             elif row[j] != " " and random.random() < 0.4:  # Remove spaces to maintain width
                 row[j] = " "
         for j in range(22, 27):  # Add debris on the other side
             if random.random() < 0.3:
-                debris_char = random.choice(['.', ',', ':', ';', "'", "\""])
-                row[j] = debris_char
+                row[j] = random.choice([".", ",", ":", ";", "'", "\""])
             elif row[j] != " " and random.random() < 0.4:  # Remove spaces to maintain width
                 row[j] = " "
         image[i] = "".join(row)
@@ -44,11 +40,11 @@ def run_miner():
         "         |      |///|      |                           ",
         "         |      |///|      |                           ",
         "         |      |///|      |                           ",
-        "         |      /+++\      |                           ",
+        "         |      |___|      |                           ",
         "         |     |=====|     |                           ",
         "         |      \===/      |                           ",
-        "         \       \=/       /                           ",
-        "          \       V       /                            "
+        "         |       \=/       |                           ",
+        "         \        V        /                           "
     ]
 
     # Define variables
@@ -62,12 +58,14 @@ def run_miner():
 
         drill_image[1] = f"         |      |///|      |   MINING IN PROGRESS  "
         drill_image[1] += f"  Percentage: {x*10:2d}%       "
-        drill_image[2] = f"         |      |///|      |   ORE COLLECTED: "
-        drill_image[2] += f"{y}"
+        drill_image[2] = f"         |      |///|      |   ORE NAME: "
+        drill_image[2] += f"{ore_name}"
+        drill_image[3] = f"         |      |///|      |   ORE COLLECTED: "
+        drill_image[3] += f"{y}"
 
         display_image(drill_image)
         add_debris(drill_image, debrisZoneX, debrisZoneY)
-        time.sleep(0.5)  # Adjust the sleep time for desired speed
+        time.sleep(0.1)  # Adjust the sleep time for desired speed
         #drill_image.insert(0, " " * 16 + "|///|" + " " * 16)  # Add a new line at the top
 
         hitChance = random.random()
@@ -85,7 +83,10 @@ def run_miner():
             display_image(drill_image)
             add_debris(drill_image, debrisZoneX, debrisZoneY)
             finished = True
-            print("finished")
+            print("# Ore extraction complete #")
+            input("# Press ENTER to Continue #")
+
+        
 
     return y, ore_name
 
